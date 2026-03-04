a7cb3ee97ca157fab3b00526af552f98d866836f
ez-umib <erzazejn@umib.net>
2026-03-05 00:48:35 +0200

    Initial commit Quick-Calc

---
+++ tests/test_integration.py
@@ -0,0 +1,11 @@
+from app.calculator import Calculator
+
+def test_full_addition_flow():
+    calc = Calculator()
+    result = calc.add(5, 3)
+    assert result == 8
+
+def test_clear_function():
+    calc = Calculator()
+    result = calc.clear()
+    assert result == 0
